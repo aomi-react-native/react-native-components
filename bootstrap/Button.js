bf5e7abbd6bfa92002b2f3aa15c289045220a11a
Sean <sean.snow@live.com>
2016-05-05 20:06:21 +0800

    颜色定义

--- bootstrap/Button.js
+++ bootstrap/Button.js
@@ -1,9 +1,43 @@
-import React, {
-    Component,
-    PropTypes,
-    TouchableOpacity,
-    View
+import React, {Component, PropTypes} from 'react';
+import {
+    View,
+    Text
 } from 'react-native';
+
+import Icon from '../Icon';
+import BaseButton from '../Button';
+import Colors from './Colors';
+
+
+const styles = {
+    button: {
+        borderRadius: 5,
+        height: 44,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden'
+    },
+    link: {},
+    default: {
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ccc'
+    },
+    buttonText: {
+        margin: 5,
+        textAlign: 'center'
+    },
+    textDefault: {
+        color: '#333'
+    },
+    textOther: {
+        color: '#FFF'
+    },
+    textDisabled: {
+        color: '#CCC'
+    }
+};
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com)
@@ -12,27 +46,126 @@ import React, {
 class Button extends Component {
 
     static propTypes = {
+        /**
+         * 内容后图标
+         */
+        afterIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+        /**
+         * 内容前图标
+         */
+        beforeIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+        /**
+         * bootstrap 风格
+         */
         bsStyle: PropTypes.string,
-        disabled: PropTypes.bool
+        children: PropTypes.any,
+        color: PropTypes.string,
+        disabled: PropTypes.bool,
+        fontSize: PropTypes.number,
+        iconProps: PropTypes.object,
+        style: View.propTypes.style
     };
 
     static defaultProps = {
-        bsStyle: 'default'
+        bsStyle: 'default',
+        fontSize: 16,
+        iconProps: {}
     };
 
-    render() {
+    constructor(props) {
+        super(props);
+        ['renderContent']
+            .forEach(f => this[f] = this[f].bind(this));
+    }
 
+    renderContent() {
         let {
             disabled,
-            ...other
+            afterIcon,
+            beforeIcon,
+            iconProps,
+            fontSize,
+            children,
+            bsStyle,
+            color
         } = this.props;
 
-        let Comp = disabled ? Viewe : TouchableOpacity;
+        let newChild = [],
+            iconStyle = {
+                marginRight: 5,
+                marginLeft: 5
+            };
+        let textStyle = Object.assign({}, bsStyle === 'default' || bsStyle === 'link' ? styles.textDefault : styles.textOther);
+        if (disabled) {
+            textStyle = Object.assign({}, styles.textDisabled);
+        }
+        if (color) {
+            textStyle.color = color;
+        }
 
+        if (beforeIcon) {
+            if (typeof beforeIcon === 'string') {
+                newChild.push(
+                    <Icon {...iconProps}
+                        key="beforeIcon"
+                        name={beforeIcon}
+                        size={fontSize}
+                        style={[iconStyle,textStyle,iconProps.style]}
+                    />
+                );
+            } else {
+                newChild.push(beforeIcon);
+            }
+        }
+        if (children) {
+            if (typeof children === 'string' ||
+                (Array.isArray(children) && typeof children[0] === 'string')) {
+                newChild.push(
+                    <Text key="text"
+                          style={[{fontSize},textStyle]}>
+                        {Array.isArray(children) ? children[0] : children}
+                    </Text>
+                );
+            } else {
+                newChild.push(children);
+            }
+        }
+        if (afterIcon) {
+            if (typeof afterIcon === 'string') {
+                newChild.push(
+                    <Icon {...iconProps}
+                        key="afterIcon"
+                        name={afterIcon}
+                        size={fontSize}
+                        style={[iconStyle,textStyle,iconProps.style]}
+                    />
+                );
+            } else {
+                newChild.push(afterIcon);
+            }
+        }
+        return newChild;
+    }
+
+    render() {
+        let {bsStyle, style, ...other} = this.props;
+        let temp = [];
+        if (bsStyle === 'link') {
+            temp.push(styles.link);
+        } else {
+            temp.push(styles.button);
+            temp.push({backgroundColor: Colors[bsStyle]});
+            temp.push(styles[bsStyle] || {});
+        }
+        temp.push(style);
         return (
-            <Comp {...other}>
-            </Comp>
+            <BaseButton {...other}
+                renderContent={this.renderContent}
+                style={temp}
+            />
         );
+
+
     }
 
 }
