fb677d508b62828ea5adc09bf72083519556f450
Sean <sean.snow@live.com>
2016-05-25 10:53:06 +0800

    改造和优化

--- form/Form.js
+++ form/Form.js
@@ -1,6 +1,12 @@
-import React, {Component, PropTypes, Children, isValidElement, cloneElement} from 'react';
+import React, {
+  Component,
+  PropTypes,
+  Children,
+  isValidElement,
+  cloneElement
+} from 'react';
 import {
-    View
+  View
 } from 'react-native';
 
 import AbstractFormComponent from './AbstractFormComponent';
@@ -12,113 +18,115 @@ import AbstractFormComponent from './AbstractFormComponent';
  */
 class Form extends Component {
 
-    static propTypes = {
-        children: PropTypes.node
+  static propTypes = {
+    children: PropTypes.node
+  };
+
+  /**
+   * 错误字段
+   * @type {Array}
+   */
+  errorFields = [];
+
+  /**
+   * 缺失字段
+   * @type {Array}
+   */
+  missFields = [];
+
+  /**
+   * 表单数据
+   * @type {{}}
+   */
+  formData = {};
+
+  formFields = {};
+
+  putFormValue(name, value) {
+    this.formData[name] = value;
+  }
+
+  putErrorField(name) {
+    this.errorFields.push(name);
+  }
+
+  putMissField(name) {
+    this.missFields.push(name);
+  }
+
+  /**
+   * 删除表单错误字段
+   * @param name 字段名字
+   */
+  deleteErrOrMissField(name) {
+    let miss = this.missFields.indexOf(name);
+    miss !== -1 && this.missFields.splice(miss, 1);
+    let error = this.errorFields.indexOf(name);
+    error !== -1 && this.errorFields.splice(error, 1);
+  }
+
+  /**
+   * 获取表单数据
+   * @returns {{}}
+   */
+  getFormData() {
+    return this.formData;
+  }
+
+  /**
+   * 获取表单错误字段信息
+   * @returns {{miss: Array, error: Array}}
+   */
+  getErrorFields() {
+    return {
+      miss: Object.keys(this.missFields),
+      error: Object.keys(this.errorFields)
     };
+  }
 
-    /**
-     * 错误字段
-     * @type {{}}
-     */
-    errorFields = {};
+  /**
+   * 校验表单是否正确
+   * @returns {boolean}
+   */
+  isValid() {
+    Object.keys(this.formFields).forEach(field => {
+      this.formFields[field].valid && this.formFields[field].valid();
+    });
 
-    /**
-     * 缺失字段
-     * @type {{}}
-     */
-    missFields = {};
-
-    /**
-     * 表单数据
-     * @type {{}}
-     */
-    formData = {};
-
-    formFields = {};
-
-    putFormValue(name, value) {
-        this.formData[name] = value;
-    }
-
-    putErrorField(name) {
-        this.errorFields[name] = null;
-    }
-
-    putMissField(name) {
-        this.missFields[name] = null;
-    }
-
-    /**
-     * 删除表单错误字段
-     * @param name 字段名字
-     */
-    deleteErrOrMissField(name) {
-        this.errorFields[name] && delete this.errorFields[name];
-        this.missFields[name] && delete this.missFields[name];
-    }
-
-    /**
-     * 获取表单数据
-     * @returns {{}}
-     */
-    getFormData() {
-        return this.formData;
-    }
-
-    /**
-     * 获取表单错误字段信息
-     * @returns {{miss: Array, error: Array}}
-     */
-    getErrorFields() {
-        return {
-            miss: Object.keys(this.missFields),
-            error: Object.keys(this.errorFields)
-        };
-    }
-
-    /**
-     * 校验表单是否正确
-     * @returns {boolean}
-     */
-    isValid() {
-        Object.keys(this.formFields).forEach(field => {
-            this.formFields[field].valid && this.formFields[field].valid();
-        });
-
-        return Object.keys(this.missFields).length === 0 && Object.keys(this.errorFields).length === 0;
-    }
+    return Object.keys(this.missFields).length === 0 && Object.keys(this.errorFields).length === 0;
+  }
 
 
-    renderChildren(children) {
-        return Children.map(children, (child, index) => {
-            if (!isValidElement(child)) {
-                return child;
-            }
-            let fieldName = child.props.name;
+  renderChildren(children) {
+    return Children.map(children, (child, index) => {
+      if (!isValidElement(child)) {
+        return child;
+      }
+      let fieldName = child.props.name;
 
-            let newProps = {
-                key: index
-            };
-            if (fieldName && child.type.__proto__ === AbstractFormComponent) {
-                newProps.form = this;
-                this.formFields[fieldName] = child;
-            }
+      let newProps = {
+        key: index
+      };
+      if (fieldName && child.type.__proto__ === AbstractFormComponent) {
+        newProps.form = this;
+        newProps.ref = field => this.formFields[fieldName] = field;
+      }
 
-            newProps.children = this.renderChildren(child.props.children);
+      newProps.children = this.renderChildren(child.props.children);
 
-            return cloneElement(child, newProps);
-        });
-    }
+      return cloneElement(child, newProps);
+    });
+  }
 
-    render() {
-        let {children, ...other} = this.props;
-        return (
-            <View {...other}
-                ref="form">
-                {this.renderChildren(children)}
-            </View>
-        );
-    }
+  render() {
+    let {children, ...other} = this.props;
+    return (
+      <View {...other}
+        ref="form">
+        {this.renderChildren(children)}
+      </View>
+    );
+  }
 }
 
 export default Form;
--- form/Input.js
+++ form/Input.js
@@ -1,254 +1,254 @@
 import React, {
-    PropTypes
+  PropTypes
 } from 'react';
 
 import AbstractFormComponent from './AbstractFormComponent';
 
 import {
-    TextInput,
-    View,
-    StyleSheet
+  TextInput,
+  View,
+  StyleSheet
 } from 'react-native';
 
 import Icon from '../Icon';
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        borderWidth: 1,
-        borderColor: '#CCC'
-    },
-    icon: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 35
-    },
-    input: {
-        flex: 1,
-        height: 35,
-        padding: 4
-    }
+  container: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#CCC'
+  },
+  icon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 35
+  },
+  input: {
+    flex: 1,
+    height: 35,
+    padding: 4
+  }
 });
 const INPUT_PROPS_KEYS = [
-    'autoCapitalize',
-    'autoCorrect',
-    'autoFocus',
-    'defaultValue',
-    'editable',
-    'keyboardType',
-    'maxLength',
-    'multiline',
-    'onBlur',
-    'onChange',
-    'onChangeText',
-    'onEndEditing',
-    'onFocus',
-    'onSubmitEditing',
-    'placeholder',
-    'placeholderTextColor',
-    'secureTextEntry',
-    'value',
-    'blurOnSubmit',
-    'clearButtonMode',
-    'clearTextOnFocus',
-    'enablesReturnKeyAutomatically',
-    'keyboardAppearance',
-    'numberOfLines',
-    'onKeyPress',
-    'returnKeyType',
-    'selectTextOnFocus',
-    'selectionState',
-    'textAlign',
-    'underlineColorAndroid',
-    'name'
+  'autoCapitalize',
+  'autoCorrect',
+  'autoFocus',
+  'defaultValue',
+  'editable',
+  'keyboardType',
+  'maxLength',
+  'multiline',
+  'onBlur',
+  'onChange',
+  'onChangeText',
+  'onEndEditing',
+  'onFocus',
+  'onSubmitEditing',
+  'placeholder',
+  'placeholderTextColor',
+  'secureTextEntry',
+  'value',
+  'blurOnSubmit',
+  'clearButtonMode',
+  'clearTextOnFocus',
+  'enablesReturnKeyAutomatically',
+  'keyboardAppearance',
+  'numberOfLines',
+  'onKeyPress',
+  'returnKeyType',
+  'selectTextOnFocus',
+  'selectionState',
+  'textAlign',
+  'underlineColorAndroid',
+  'name'
 ];
 class Input extends AbstractFormComponent {
 
-    static propTypes = {
-        /**
-         * Input 之后,如果为String  则显示Icon
-         */
-        after: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-        /**
-         * Input 之前,如果为String 则显示Icon
-         */
-        before: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-        /**
-         * 自定义value验证
-         */
-        customValid: PropTypes.func,
-        /**
-         * input 默认值
-         */
-        defaultValue: PropTypes.string,
-        /**
-         * Input 所属表单
-         */
-        form: PropTypes.object,
-        /**
-         * 图标属性
-         */
-        iconProps: PropTypes.object,
-        /**
-         * 图标样式
-         */
-        iconStyle: View.propTypes.style,
-        /**
-         * 输入框样式
-         */
-        inputStyle: TextInput.propTypes.style,
-        /**
-         * 字段名字,用于提交表单获取值
-         */
-        name: PropTypes.string,
-        /**
-         * input 值更改回调函数
-         */
-        onChangeText: PropTypes.func,
-        /**
-         * input value 验证正则表达式
-         */
-        pattern: PropTypes.string,
-        /**
-         * 表明表单的值是必须的
-         */
-        required: PropTypes.bool,
-        /**
-         * container 样式
-         */
-        style: View.propTypes.style,
-        /**
-         * Android input 下划线
-         */
-        underlineColorAndroid: PropTypes.string,
-        /**
-         * 是否验证input的值
-         */
-        validate: PropTypes.bool,
-        /**
-         * input 值
-         */
-        value: PropTypes.string
-    };
+  static propTypes = {
+    /**
+     * Input 之后,如果为String  则显示Icon
+     */
+    after: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    /**
+     * Input 之前,如果为String 则显示Icon
+     */
+    before: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    /**
+     * 自定义value验证
+     */
+    customValid: PropTypes.func,
+    /**
+     * input 默认值
+     */
+    defaultValue: PropTypes.string,
+    /**
+     * Input 所属表单
+     */
+    form: PropTypes.object,
+    /**
+     * 图标属性
+     */
+    iconProps: PropTypes.object,
+    /**
+     * 图标样式
+     */
+    iconStyle: View.propTypes.style,
+    /**
+     * 输入框样式
+     */
+    inputStyle: TextInput.propTypes.style,
+    /**
+     * 字段名字,用于提交表单获取值
+     */
+    name: PropTypes.string,
+    /**
+     * input 值更改回调函数
+     */
+    onChangeText: PropTypes.func,
+    /**
+     * input value 验证正则表达式
+     */
+    pattern: PropTypes.string,
+    /**
+     * 表明表单的值是必须的
+     */
+    required: PropTypes.bool,
+    /**
+     * container 样式
+     */
+    style: View.propTypes.style,
+    /**
+     * Android input 下划线
+     */
+    underlineColorAndroid: PropTypes.string,
+    /**
+     * 是否验证input的值
+     */
+    validate: PropTypes.bool,
+    /**
+     * input 值
+     */
+    value: PropTypes.string
+  };
 
-    static defaultProps = {
-        defaultValue: '',
-        iconProps: {},
-        underlineColorAndroid: 'transparent',
-        validate: true,
-        value: ''
-    };
+  static defaultProps = {
+    defaultValue: '',
+    iconProps: {},
+    underlineColorAndroid: 'transparent',
+    validate: true,
+    value: ''
+  };
 
-    constructor(props) {
-        super(props);
-        ['onChangeText'].forEach(f => this[f] = this[f].bind(this));
-        let {name, form} = props;
-        name && form && form.putFormValue(name, props.value || props.defaultValue);
+  constructor(props) {
+    super(props);
+    ['onChangeText'].forEach(f => this[f] = this[f].bind(this));
+    let {name, form} = props;
+    name && form && form.putFormValue(name, props.value || props.defaultValue);
+  }
+
+  state = {
+    value: this.props.value || this.props.defaultValue
+  };
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.value !== this.props.value) {
+      this.onChangeText(nextProps.value);
     }
+  }
 
-    state = {
-        value: this.props.value || this.props.defaultValue
-    };
-
-    componentWillUpdate(nextProps) {
-        if (nextProps.value !== this.props.value) {
-            this.onChangeText(nextProps.value);
-        }
-    }
-
-    getComp(name) {
-        let {iconProps, iconStyle} = this.props;
-        if (name) {
-            if (typeof name === 'string') {
-                return (
-                    <View style={[styles.icon,iconStyle]}>
-                        <Icon {...iconProps}
-                            name={name}
-                        />
-                    </View>
-                );
-            }
-
-            return name;
-        }
-        return null;
-    }
-
-    getValue() {
-        return this.state.value;
-    }
-
-    valid() {
-        let {validate, customValid, pattern, required, name, form} = this.props;
-        if (validate) {
-            if (customValid) {
-                let result = customValid(this.state.value);
-                this.setFormFieldInfo(result);
-                return result;
-            }
-
-            if (required) {
-                let result = this.state.value !== '';
-                if (result) {
-                    name && form && form.deleteErrOrMissField(name);
-                } else {
-                    name && form && form.putMissField(name);
-                    return result;
-                }
-            }
-
-            if (pattern) {
-                let reg = new RegExp(pattern);
-                let result = reg.test(this.state.value);
-                this.setFormFieldInfo(result);
-                return result;
-            }
-        }
-
-        return true;
-    }
-
-    setFormFieldInfo(result) {
-        let {name, form} = this.props;
-        if (result) {
-            name && form && form.deleteErrOrMissField(name);
-        } else {
-            name && form && form.putErrorField(name);
-        }
-    }
-
-    onChangeText(value) {
-        this.setState({value});
-        let {onChangeText, name, form} = this.props;
-        name && form && form.putFormValue(name, value);
-        onChangeText && onChangeText(value);
-    }
-
-    render() {
-        let {before, after, style, inputStyle, ...other} = this.props;
-
-        let newInputProps = {},
-            newProps = Object.assign({}, other);
-
-        INPUT_PROPS_KEYS.forEach(key => {
-            newInputProps[key] = this.props[key];
-            newProps[key] && delete newProps[key];
-        });
-
-
+  getComp(name) {
+    let {iconProps, iconStyle} = this.props;
+    if (name) {
+      if (typeof name === 'string') {
         return (
-            <View {...newProps}
-                style={[styles.container, style]}>
-                {this.getComp(before)}
-                <TextInput {...newInputProps}
-                    onChangeText={this.onChangeText}
-                    style={[styles.input, style, inputStyle]}
-                    value={this.state.value}
-                />
-                {this.getComp(after)}
-            </View>
+          <View style={[styles.icon,iconStyle]}>
+            <Icon {...iconProps}
+              name={name}
+            />
+          </View>
         );
+      }
+
+      return name;
     }
+    return null;
+  }
+
+  getValue() {
+    return this.state.value;
+  }
+
+  valid() {
+    let {validate, customValid, pattern, required, name, form} = this.props;
+    if (validate) {
+      if (customValid) {
+        let result = customValid(this.state.value);
+        this.setFormFieldInfo(result);
+        return result;
+      }
+
+      if (required) {
+        if (this.state.value === '') {
+          name && form && form.putMissField(name);
+          return false;
+        } else {
+          name && form && form.deleteErrOrMissField(name);
+          return true;
+        }
+      }
+
+      if (pattern) {
+        let reg = new RegExp(pattern);
+        let result = reg.test(this.state.value);
+        this.setFormFieldInfo(result);
+        return result;
+      }
+    }
+
+    return true;
+  }
+
+  setFormFieldInfo(result) {
+    let {name, form} = this.props;
+    if (result) {
+      name && form && form.deleteErrOrMissField(name);
+    } else {
+      name && form && form.putErrorField(name);
+    }
+  }
+
+  onChangeText(value) {
+    this.setState({value});
+    let {onChangeText, name, form} = this.props;
+    name && form && form.putFormValue(name, value);
+    onChangeText && onChangeText(value);
+  }
+
+  render() {
+    let {before, after, style, inputStyle, ...other} = this.props;
+
+    let newInputProps = {},
+      newProps = Object.assign({}, other);
+
+    INPUT_PROPS_KEYS.forEach(key => {
+      newInputProps[key] = this.props[key];
+      newProps[key] && delete newProps[key];
+    });
+
+
+    return (
+      <View {...newProps}
+        style={[styles.container, style]}>
+        {this.getComp(before)}
+        <TextInput {...newInputProps}
+          onChangeText={this.onChangeText}
+          style={[styles.input, style, inputStyle]}
+          value={this.state.value}
+        />
+        {this.getComp(after)}
+      </View>
+    );
+  }
 
 }
 
