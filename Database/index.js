04a646fa5371d862d936db7e71f4429b3c3f3b85
Sean <sean.snow@live.com>
2016-06-08 15:00:52 +0800

    Database

---
+++ Database/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 16/6/8
+ */
+
+import Database from './Database';
+import Collection from './Collection';
+
+export {
+  Database as default,
+  Collection
+};
