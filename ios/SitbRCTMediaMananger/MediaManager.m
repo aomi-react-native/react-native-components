986d0dcab15f0ab812ebee2b960bba11af7480ed
Sean <sean.snow@live.com>
2016-07-04 12:58:56 +0800

    图片方向和尺寸

--- ios/SitbRCTMediaMananger/MediaManager.m
+++ ios/SitbRCTMediaMananger/MediaManager.m
@@ -21,18 +21,27 @@ RCT_EXPORT_MODULE(SitbRCTMediaManager)
 /** 常量 */
 - (NSDictionary<NSString *, id> *)constantsToExport {
     return @{
-            @"sourceType" : @{
+            @"SourceType" : @{
                     @"photoLibrary" : @(UIImagePickerControllerSourceTypePhotoLibrary),
                     @"savedPhotosAlbum" : @(UIImagePickerControllerSourceTypeSavedPhotosAlbum),
                     @"camera" : @(UIImagePickerControllerSourceTypeCamera)
             },
-            @"mediaType" : @{
-                    @"Image" : @(MediaTypeImage),
-                    @"Video" : @(MediaTypeVideo)
+            @"MediaType" : @{
+                    @"image" : @(MediaTypeImage),
+                    @"video" : @(MediaTypeVideo)
             },
-            @"cameraType" : @{
+            @"CameraType" : @{
                     @"front" : @(UIImagePickerControllerCameraDeviceFront),
                     @"back" : @(UIImagePickerControllerCameraDeviceRear)
+            },
+            @"Quality" : @{
+                    @"high" : @(UIImagePickerControllerQualityTypeHigh),
+                    @"medium" : @(UIImagePickerControllerQualityTypeMedium),
+                    @"low" : @(UIImagePickerControllerQualityTypeLow),
+                    @"VGA640x480" : @(UIImagePickerControllerQualityType640x480),
+                    @"VGA1280x720" : @(UIImagePickerControllerQualityTypeIFrame1280x720),
+                    @"VGA960x540" : @(UIImagePickerControllerQualityTypeIFrame960x540)
+
             }
     };
 }
@@ -75,6 +84,10 @@ RCT_EXPORT_METHOD(
 }
 
 
+/**
+ * 根据指定的参数启动相机
+ * @param options 相机启动参数
+ */
 - (void)launchWithOptions:(NSDictionary *)options {
     self.pickerController = [[UIImagePickerController alloc] init];
     UIImagePickerControllerSourceType sourceType = (UIImagePickerControllerSourceType) ((NSNumber *) [options valueForKey:@"sourceType"]).intValue;
@@ -84,15 +97,18 @@ RCT_EXPORT_METHOD(
         self.pickerController.cameraDevice = cameraDevice;
     }
 
-
     MediaType mediaType = (MediaType) ((NSNumber *) options[@"mediaType"]).intValue;
     switch (mediaType) {
-        case MediaTypeImage:
+        case MediaTypeImage: {
             self.pickerController.mediaTypes = @[(NSString *) kUTTypeImage];
             break;
-        case MediaTypeVideo:
+        }
+        case MediaTypeVideo: {
             self.pickerController.mediaTypes = @[(NSString *) kUTTypeVideo];
+            UIImagePickerControllerQualityType quality = (UIImagePickerControllerQualityType) ((NSNumber *) options[@"quality"]).intValue;
+            self.pickerController.videoQuality = quality;
             break;
+        }
     }
 
     self.pickerController.allowsEditing = [options[@"allowsEditing"] boolValue];
@@ -127,17 +143,59 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
 
             UIImage *image = info[@"UIImagePickerControllerOriginalImage"];
 
-            NSDictionary *cameraMetadata = info[@"UIImagePickerControllerMediaMetadata"];
-            NSDictionary *requestMetadata = self.options[@"metadata"];
+            UIImagePickerControllerQualityType quality = (UIImagePickerControllerQualityType) ((NSNumber *) self.options[@"quality"]).intValue;
 
-            NSMutableDictionary *metadata = [[cameraMetadata copy] mutableCopy];
-            [metadata addEntriesFromDictionary:requestMetadata];
-            [metadata setGpsMetadata:requestMetadata];
+            CGFloat qualityFloat = 1.0F;
+
+            switch (quality) {
+                case UIImagePickerControllerQualityTypeMedium:
+                    qualityFloat /= 2;
+                    break;
+                case UIImagePickerControllerQualityTypeLow:
+                    qualityFloat /= 3;
+                    break;
+                default:
+                    break;
+            }
+
+            NSData *imageData = UIImageJPEGRepresentation(image, qualityFloat);
+            CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef) imageData, NULL);
+            NSMutableDictionary *imageMetadata = [(NSDictionary *) CFBridgingRelease(CGImageSourceCopyPropertiesAtIndex(source, 0, NULL)) mutableCopy];
+            CGImageRef CGImage = CGImageSourceCreateImageAtIndex(source, 0, NULL);
+            CGImageRef rotatedCGImage;
+            int metadataOrientation = [imageMetadata[(NSString *) kCGImagePropertyOrientation] intValue];
+            if (metadataOrientation == 6) {
+                rotatedCGImage = [self newCGImageRotatedByAngle:CGImage angle:270];
+            } else if (metadataOrientation == 1) {
+                rotatedCGImage = [self newCGImageRotatedByAngle:CGImage angle:0];
+            } else if (metadataOrientation == 3) {
+                rotatedCGImage = [self newCGImageRotatedByAngle:CGImage angle:180];
+            } else {
+                rotatedCGImage = [self newCGImageRotatedByAngle:CGImage angle:0];
+            }
+            CGImageRelease(CGImage);
+            // Erase metadata orientation
+            [imageMetadata removeObjectForKey:(NSString *) kCGImagePropertyOrientation];
+            // Erase stupid TIFF stuff
+            [imageMetadata removeObjectForKey:(NSString *) kCGImagePropertyTIFFDictionary];
+            // Set GPS
+            [imageMetadata setGpsMetadata:self.options[@"metadata"]];
+            [imageMetadata addEntriesFromDictionary:self.options[@"metadata"]];
+
+            // Create destination thing
+            NSMutableData *rotatedImageData = [NSMutableData data];
+            CGImageDestinationRef destination = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)rotatedImageData, CGImageSourceGetType(source), 1, NULL);
+            CFRelease(source);
+            // add the image to the destination, reattaching metadata
+            CGImageDestinationAddImage(destination, rotatedCGImage, (__bridge CFDictionaryRef) imageMetadata);
+            // And write
+            CGImageDestinationFinalize(destination);
+            CFRelease(destination);
 
             // It's unclear if writeImageToSavedPhotosAlbum is thread-safe
             dispatch_async(dispatch_get_main_queue(), ^{
                 ALAssetsLibrary *assets = [[ALAssetsLibrary alloc] init];
-                [assets writeImageToSavedPhotosAlbum:image.CGImage metadata:metadata completionBlock:^(NSURL *assetURL, NSError *saveError) {
+                [assets writeImageToSavedPhotosAlbum:rotatedCGImage metadata:imageMetadata completionBlock:^(NSURL *assetURL, NSError *saveError) {
                     if (saveError) {
                         self.reject(@"ERROR", @"保存图片失败", saveError);
                     } else {
@@ -191,7 +249,33 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
     return imageTempFile;
 }
 
+- (CGImageRef)newCGImageRotatedByAngle:(CGImageRef)imgRef angle:(CGFloat)angle {
+    CGFloat angleInRadians = (CGFloat) (angle * (M_PI / 180));
+    CGFloat width = CGImageGetWidth(imgRef);
+    CGFloat height = CGImageGetHeight(imgRef);
+
+    CGRect imgRect = CGRectMake(0, 0, width, height);
+    CGAffineTransform transform = CGAffineTransformMakeRotation(angleInRadians);
+    CGRect rotatedRect = CGRectApplyAffineTransform(imgRect, transform);
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef bmContext = CGBitmapContextCreate(NULL, (size_t) rotatedRect.size.width, (size_t) rotatedRect.size.height, 8, 0, colorSpace, (CGBitmapInfo) kCGImageAlphaPremultipliedFirst);
+
+    CGContextSetAllowsAntialiasing(bmContext, TRUE);
+    CGContextSetInterpolationQuality(bmContext, kCGInterpolationNone);
+
+    CGColorSpaceRelease(colorSpace);
+
+    CGContextTranslateCTM(bmContext, +(rotatedRect.size.width / 2), +(rotatedRect.size.height / 2));
+    CGContextRotateCTM(bmContext, angleInRadians);
+    CGContextTranslateCTM(bmContext, -(rotatedRect.size.width / 2), -(rotatedRect.size.height / 2));
+
+    CGContextDrawImage(bmContext, CGRectMake((rotatedRect.size.width - width) / 2.0f, (rotatedRect.size.height - height) / 2.0f, width, height), imgRef);
+
+    CGImageRef rotatedImage = CGBitmapContextCreateImage(bmContext);
+    CFRelease(bmContext);
+    return rotatedImage;
+}
+
+
 @end
-
-
-#pragma clang diagnostic pop
