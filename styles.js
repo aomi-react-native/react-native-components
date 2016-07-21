ecba7aaefba31011ee98e32f0126a72fa9c22cb4
Sean <sean.snow@live.com>
2016-07-21 09:55:47 +0800

    公共样式定义

---
+++ styles.js
@@ -0,0 +1,14 @@
+/**
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 16/7/20
+ */
+
+export default {
+  fullScreenAbsolute: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
+};
