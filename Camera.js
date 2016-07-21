774fdf0467680a3ce808859c74d7b14315391a73
Sean <sean.snow@live.com>
2016-07-21 09:56:05 +0800

    代码优化

--- Camera.js
+++ Camera.js
@@ -3,17 +3,22 @@ import Component from './AbstractComponent';
 import {
   requireNativeComponent,
   View,
-  NativeModules
+  NativeModules,
+  UIManager,
+  Platform
 } from 'react-native';
 
 
-const {
-  SitbCameraModule: {
-    CameraType,
-    Orientation,
-    capture
-  }
-} = NativeModules;
+// const {
+//   SitbCameraModule: {
+//     CameraType,
+//     Orientation,
+//     capture
+//   }
+// } = NativeModules;
+
+
+let Constants, CameraFacing, Orientation, RCTCamera;
 
 
 /**
@@ -24,32 +29,59 @@ class Camera extends Component {
 
   static propTypes = {
     ...View.propTypes,
-    orientation: PropTypes.number,
-    type: PropTypes.number
+    /**
+     * 相机方向
+     */
+    cameraFacing: PropTypes.number,
+    /**
+     * 安卓相机版本
+     * 4 使用
+     * 5 安卓5.0相机
+     */
+    cameraVersion: PropTypes.oneOf([4, 5]),
+    orientation: PropTypes.number
   };
 
   static defaultProps = {
-    orientation: Orientation.auto,
-    type: CameraType.back
+    // cameraFacing: CameraFacing.back,
+    // orientation: Orientation.auto,
+    cameraVersion: 4
   };
+
+  constructor(props) {
+    super(props);
+    if (Platform.OS === 'android') {
+      if (props.cameraVersion === 4) {
+        Constants = UIManager.SitbRCTCameraView.Constants;
+        RCTCamera = requireNativeComponent('SitbRCTCameraView', Camera);
+      } else if (props.cameraVersion === 5) {
+        Constants = UIManager.SitbRCTCamera2View.Constants;
+        RCTCamera = requireNativeComponent('SitbRCTCamera2View', Camera);
+      }
+      CameraFacing = Constants.CameraFacing;
+      Orientation = Constants.Orientation;
+    } else {
+      RCTCamera = requireNativeComponent('SitbRCTCameraView', Camera);
+    }
+  }
 
   capture(option) {
     return capture(option);
   }
 
   render() {
+    const {cameraFacing, orientation, ...other} = this.props;
     return (
-      <RCTCamera {...this.props} />
+      <RCTCamera {...other}
+        cameraFacing={cameraFacing || CameraFacing.back}
+        orientation={orientation || Orientation.auto}
+      />
     );
   }
 
 }
 
-const RCTCamera = requireNativeComponent('RCTSitbCameraView', Camera);
-
 
 export {
-  Camera as default,
-  CameraType,
-  Orientation
+  Camera as default
 };
