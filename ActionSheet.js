36f21f3e733ad12a82dd4f33ee31a65b83efc5eb
Sean <sean.snow@live.com>
2016-08-10 15:38:55 +0800

    优化

--- ActionSheet.js
+++ ActionSheet.js
@@ -3,13 +3,16 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  TouchableHighlight,
+  Dimensions
 } from 'react-native';
 import { View as AnimatableView } from 'react-native-animatable';
 import RootSiblings from 'react-native-root-siblings';
 import Component from './AbstractComponent';
 import Button from './bootstrap/Button';
 import Dialog from './Dialog';
+import { Colors } from './styles';
 
 const height = 50;
 
@@ -28,18 +31,20 @@ const styles = StyleSheet.create({
   },
   title: {
     height,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderColor: Colors.separator,
+    borderBottomWidth: 1
   },
   titleText: {
     color: '#8F8F91',
-    textAlign: 'center'
+    textAlign: 'center',
+    fontSize: 16
   },
   button: {
     borderRadius: 0,
     height,
-    borderTopColor: '#DADADA',
-    borderTopWidth: 1,
-    backgroundColor: '#FFF'
+    borderColor: Colors.separator,
+    borderBottomWidth: 1
   },
   cancel: {
     marginTop: 10,
@@ -102,7 +107,7 @@ class ActionSheetComponent extends Component {
     duration: 500
   };
 
-  cancelPress() {
+  handleCancelPress() {
     const {cancelPress} = this.props;
     this.close(cancelPress);
   }
@@ -132,9 +137,12 @@ class ActionSheetComponent extends Component {
       cancel
     } = this.props;
 
+    const {
+      height
+    } = Dimensions.get('window');
+
     return (
       <Dialog hideAnimation={this.hideAnimation}
-              onPress={this.switchOpen}
               showAnimation={this.showAnimation}
               statusBarAutoHidden={false}
               style={styles.container}
@@ -147,31 +155,35 @@ class ActionSheetComponent extends Component {
             <View style={styles.title}>
               <Text style={styles.titleText}>{title || 'Action'}</Text>
             </View>
-            <ScrollView alwaysBounceVertical={false}>
-              {
-                options.map((option, index)=> {
-                  let tmp = null;
-                  if (options.length - 1 === index) {
-                    tmp = {
-                      borderBottomLeftRadius: 10,
-                      borderBottomRightRadius: 10
-                    };
-                  }
-                  return (
-                    <Button color="#0977FF"
-                            key={index}
-                            onPress={this.handlePress(option.onPress)}
-                            style={[styles.button, tmp]}
-                    >
-                      {option.label}
-                    </Button>
-                  );
-                })
-              }
+            <ScrollView alwaysBounceVertical={false}
+                        style={{maxHeight: height * 0.6}}
+            >
+              {options.map((option, index)=> {
+                let tmp = null;
+                if (options.length - 1 === index) {
+                  tmp = {
+                    borderBottomLeftRadius: 10,
+                    borderBottomRightRadius: 10
+                  };
+                }
+                return (
+                  <Button Comp={TouchableHighlight}
+                          buttonStyle={[styles.button, tmp]}
+                          color="#0977FF"
+                          key={index}
+                          onPress={this.handlePress(option.onPress)}
+                          underlayColor={Colors.underlay}
+                  >
+                    {option.label}
+                  </Button>
+                );
+              })}
             </ScrollView>
           </View>
-          <Button onPress={this.cancelPress}
+          <Button Comp={TouchableHighlight}
+                  onPress={this.handleCancelPress}
                   style={[styles.content, styles.cancel]}
+                  underlayColor={Colors.underlay}
           >
             <Text style={styles.cancelText}>
               {cancel || 'Cancel'}
@@ -187,14 +199,14 @@ class ActionSheetComponent extends Component {
 class ActionSheet {
   /**
    * @param options
-   * options（字符串数组） - 一组按钮的标题（必选）
+   * options (对象数组) - 一组按钮的选择（必选）{label: '按钮标题', onPress: ()=>console.log('press')}
    * cancelButtonIndex（整型） - 选项中取消按钮所在的位置（索引）
    * destructiveButtonIndex（整型） - 选项中删除按钮所在的位置（索引）
    * title（字符串） - 弹出框顶部的标题
    * message（字符串） - 弹出框顶部标题下方的信息
    * @param callback
    */
-  static showActionSheetWithOptions(options:Object, callback:Function) {
+  static showActionSheetWithOptions(options: Object, callback: Function) {
     let actionSheet = new RootSiblings(<View />);
     actionSheet.update(
       <ActionSheetComponent {...options}
