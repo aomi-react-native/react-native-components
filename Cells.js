e7b1cfafba932d8acfbfd2bb2908f5debb1d0971
Sean <sean.snow@live.com>
2017-03-08 14:29:05 +0800

    Cells 替换换来的SettingList

---
+++ Cells.js
@@ -0,0 +1,151 @@
+import React, { PropTypes } from 'react';
+import { View, Text, StyleSheet, TouchableHighlight, ListView } from 'react-native';
+import AbstractComponent from './AbstractComponent';
+import Icon from './Icon';
+import Button from './Button';
+import { Colors } from './styles';
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.underlay,
+    borderTopWidth: 1,
+    borderTopColor: Colors.underlay
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+    marginLeft: 15,
+    paddingRight: 15
+  },
+  header: {
+    width: 30
+  },
+  body: {
+    flex: 1
+  },
+  text: {
+    color: '#000'
+  },
+  separator: {
+    height: 0.5,
+    marginLeft: 15
+  }
+});
+
+/**
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 16/5/25
+ */
+class List extends AbstractComponent {
+
+  static propTypes = {
+    cellStyle: View.propTypes.style,
+    items: PropTypes.array,
+    separatorColor: PropTypes.string,
+    onItemPress: PropTypes.func
+  };
+
+  static defaultProps = {
+    alwaysBounceVertical: false,
+    rowHasChanged: (r1, r2) => r1 !== r2,
+    separatorColor: Colors.separator
+  };
+
+  state = {};
+
+  constructor(props) {
+    super(props);
+    const {items, rowHasChanged} = props;
+    const ds = new ListView.DataSource({rowHasChanged});
+    this.state = {
+      dataSource: ds.cloneWithRows(items)
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.items !== nextProps.items) {
+      this.setState({dataSource: this.state.dataSource.cloneWithRows(nextProps.items)});
+    }
+  }
+
+  renderHeader(header) {
+    return header || <View />;
+  }
+
+  renderBody(body) {
+    if (!body || typeof body === 'string') {
+      return (
+        <Text style={styles.body}>{body || ''}</Text>
+      );
+    }
+    return body;
+  }
+
+  renderFooter(footer) {
+    if (footer) {
+      if (typeof footer === 'string') {
+        return (
+          <Text>{footer}</Text>
+        );
+      }
+      return footer;
+    }
+    return (
+      <Icon color="#000"
+            name="angle-right"
+            size={18}
+      />
+    );
+  }
+
+  renderRow(row, sectionID, rowID, highlightRow) {
+    const {header, body, footer} = row;
+    const {onItemPress, cellStyle} = this.props;
+    return (
+      <Button Comp={TouchableHighlight}
+              containerStyle={[styles.row, cellStyle]}
+              key={rowID}
+              onPress={() => onItemPress && onItemPress(row, sectionID, rowID)}
+              type="link"
+              underlayColor={Colors.underlay}
+      >
+        {this.renderHeader(header)}
+        {this.renderBody(body)}
+        {this.renderFooter(footer)}
+      </Button>
+    );
+  }
+
+  renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
+    const {separatorColor, items} = this.props;
+    if (rowID === `${items.length - 1}`) {
+      return null;
+    }
+    const style = {
+      backgroundColor: adjacentRowHighlighted ? 'transparent' : separatorColor
+    };
+    return (
+      <View key={`${sectionID}-${rowID}`}
+            style={[styles.separator, style]}
+      />
+    );
+  }
+
+  render() {
+    const {contentContainerStyle, ...other} = this.props;
+    return (
+      <ListView {...other}
+        contentContainerStyle={[styles.container, contentContainerStyle]}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        renderSeparator={this.renderSeparator}
+      />
+    );
+  }
+
+}
+
+export default List;
