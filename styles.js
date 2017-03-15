5153553374066f643409590ae6ace71f02cb55e1
Sean <sean.snow@live.com>
2017-03-15 15:00:45 +0800

    颜色

--- styles.js
+++ styles.js
@@ -4,9 +4,14 @@
  */
 
 export const Colors = {
-  separator: '#dfdfdf',
-  underlay: '#d0d0d0',
-  white: '#FFF'
+  separator: '#e5e5e5',
+  underlay: '#f5f5f9',
+  white: '#FFF',
+  primary: '#337ab7',
+  success: '#5cb85c',
+  info: '#5bc0de',
+  warning: '#f0ad4e',
+  danger: '#d9534f'
 };
 
 export default {
