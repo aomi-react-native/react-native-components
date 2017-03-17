087627791b529eadadadc8ae01ad9fe188195586
Sean <sean.snow@live.com>
2017-03-17 09:10:54 +0800

    相机质量定义

---
+++ android/Camera/src/main/java/software/sitb/react/camera/commons/Quality.java
@@ -0,0 +1,13 @@
+package software.sitb.react.camera.commons;
+
+/**
+ * @author Sean sean.snow@live.com createAt 2017/3/16
+ */
+public enum Quality {
+    HIGH,
+    MEDIUM,
+    LOW,
+    VGA,
+    HD720,
+    HD1080
+}
