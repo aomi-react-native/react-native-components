d82a3430d4a58c1af2b28171a57042340757fb25
Sean <sean.snow@live.com>
2017-04-20 10:20:49 +0800

    sendEvent

--- android/commons/src/main/java/software/sitb/react/commons/DefaultReactContextBaseJavaModule.java
+++ android/commons/src/main/java/software/sitb/react/commons/DefaultReactContextBaseJavaModule.java
@@ -1,14 +1,31 @@
 package software.sitb.react.commons;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 /**
  * @author 田尘殇Sean sean.snow@live.com
  */
 public abstract class DefaultReactContextBaseJavaModule extends ReactContextBaseJavaModule {
 
+    protected ReactApplicationContext reactContext;
+
     public DefaultReactContextBaseJavaModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    /**
+     * 向JS发送事件
+     *
+     * @param event 事件名称
+     * @param data  数据
+     */
+    public void sendEvent(String event, WritableMap data) {
+        DeviceEventManagerModule.RCTDeviceEventEmitter eventEmitter = reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class);
+        eventEmitter.emit(event, data);
     }
 }
