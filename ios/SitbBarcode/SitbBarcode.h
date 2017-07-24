e0cd1790503fca98311b7d6524ef9f06a1722e1d
Sean <sean.snow@live.com>
2017-07-24 17:18:32 +0800

    rename

---
+++ ios/SitbBarcode/SitbBarcode.h
@@ -0,0 +1,13 @@
+//
+//  SitbBarcode.h
+//  SitbBarcode
+//
+//  Created by 田尘殇 on 2017/7/24.
+//  Copyright © 2017年 Sitb Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SitbBarcode : NSObject
+
+@end
---
+++ ios/SitbBarcode/SitbBarcode.m
@@ -0,0 +1,13 @@
+//
+//  SitbBarcode.m
+//  SitbBarcode
+//
+//  Created by 田尘殇 on 2017/7/24.
+//  Copyright © 2017年 Sitb Software. All rights reserved.
+//
+
+#import "SitbBarcode.h"
+
+@implementation SitbBarcode
+
+@end
