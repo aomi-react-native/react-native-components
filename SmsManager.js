2a88aa0454eb0ce69913d64a88299129dd2b0356
Sean <sean.snow@live.com>
2017-07-26 11:06:24 +0800

    发送短信

---
+++ SmsManager.js
@@ -0,0 +1,21 @@
+import { NativeModules } from 'react-native';
+
+const {SitbSmsManager} = NativeModules;
+
+/**
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 2017/7/26
+ */
+export default class SmsManager {
+
+  /**
+   * 发送短信
+   * @param recipients 收件人
+   * @param body 短信内容
+   * @returns {*}
+   */
+  static sendSms(recipients: Array<String>, body: String) {
+    return SitbSmsManager.sendSms(recipients, body);
+  }
+
+}
