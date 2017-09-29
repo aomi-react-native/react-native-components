478dfbdca812f6c812ce2f5c84b51b92c18ede7f
Sean <sean.snow@live.com>
2017-09-29 17:30:42 +0800

    本地认证

---
+++ LocalAuthentication.js
@@ -0,0 +1,33 @@
+import { NativeModules } from 'react-native';
+
+const {SitbLocalAuthentication} = NativeModules;
+
+export const Code = {
+  systemCancel: 'SystemCancel',
+  userCancel: 'UserCancel',
+  userFallback: 'UserFallback',
+  exception: 'EXCEPTION'
+};
+
+/**
+ * @author 田尘殇Sean(sean.snow@live.com)
+ * @date 2017/9/29
+ */
+export default class LocalAuthentication {
+
+  /**
+   * 是否支持生物识别
+   */
+  static supportBiometrics() {
+    return SitbLocalAuthentication.supportBiometrics();
+  }
+
+  /**
+   * 验证指纹
+   * @param msg 提示消息
+   */
+  static fingerprintValidate(msg) {
+    return SitbLocalAuthentication.fingerprintValidate(msg);
+  }
+
+}
