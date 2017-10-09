65ff15e7d01e9caeda9892f1244560848550482c
Sean <sean.snow@live.com>
2017-10-09 15:05:10 +0800

    代码优化

--- android/local-authentication/src/main/java/software/sitb/react/local/authentication/LocalAuthentication.java
+++ android/local-authentication/src/main/java/software/sitb/react/local/authentication/LocalAuthentication.java
@@ -1,7 +1,14 @@
 package software.sitb.react.local.authentication;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
@@ -12,36 +19,80 @@ import software.sitb.react.commons.DefaultReactContextBaseJavaModule;
  */
 public class LocalAuthentication extends DefaultReactContextBaseJavaModule {
 
-    private static final String TAG = "SitbLocalAuthentication";
+  private static final String TAG = "SitbLocalAuthentication";
 
-    public LocalAuthentication(ReactApplicationContext reactContext) {
-        super(reactContext);
+  private Handler handler;
+
+  public LocalAuthentication(ReactApplicationContext reactContext) {
+    super(reactContext);
+    HandlerThread threadHandler = new HandlerThread("SitbLocalAuthentication");
+    threadHandler.start();
+    handler = new Handler(threadHandler.getLooper());
+  }
+
+  @Override
+  public String getName() {
+    return "SitbLocalAuthentication";
+  }
+
+  /**
+   * 是否支持指纹识别
+   *
+   * @param promise 回调结果
+   */
+  @ReactMethod
+  public void supportBiometrics(Promise promise) {
+    FingerprintManagerCompat managerCompat = FingerprintManagerCompat.from(getReactApplicationContext());
+    if (managerCompat.isHardwareDetected()) { //判断设备是否支持
+      Log.d(TAG, "设备支持指纹");
+      promise.resolve(true);
+    } else {
+      Log.w(TAG, "设备不支持指纹");
+      promise.reject("EXCEPTION", "设备不支持指纹");
+    }
+  }
+
+  @ReactMethod
+  public void fingerprintValidate(String msg, Promise promise) {
+    FingerprintManagerCompat managerCompat = FingerprintManagerCompat.from(getReactApplicationContext());
+    if (!managerCompat.hasEnrolledFingerprints()) {
+      Log.e(TAG, "没有注册指纹");
+      promise.reject("", "没有注册指纹");
+      return;
+    }
+
+    managerCompat.authenticate(null, 0, null, new AuthenticationCallback(promise), handler);
+  }
+
+  private static class AuthenticationCallback extends FingerprintManagerCompat.AuthenticationCallback {
+
+    private Promise promise;
+
+    public AuthenticationCallback(Promise promise) {
+      this.promise = promise;
     }
 
     @Override
-    public String getName() {
-        return "SitbLocalAuthentication";
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+      Log.e(TAG, "onAuthenticationError. 错误代码:" + errMsgId + ",错误消息:" + errString);
     }
 
-    /**
-     * 是否支持指纹识别
-     *
-     * @param promise 回调结果
-     */
-    @ReactMethod
-    public void supportBiometrics(Promise promise) {
-        FingerprintManagerCompat managerCompat = FingerprintManagerCompat.from(getReactApplicationContext());
-        if (managerCompat.isHardwareDetected()) { //判断设备是否支持
-            Log.d(TAG, "设备支持指纹");
-            promise.resolve(true);
-        } else {
-            Log.w(TAG, "设备不支持指纹");
-            promise.reject("EXCEPTION", "设备不支持指纹");
-        }
+    @Override
+    public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+      Log.i(TAG, "onAuthenticationHelp. 帮助代码:" + helpMsgId + ",帮助消息:" + helpString);
+      promise.reject("Help", "Help");
     }
 
-    @ReactMethod
-    public void fingerprintValidate(String msg, Promise promise) {
-
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
+      Log.d(TAG, "验证成功");
+      promise.resolve(true);
     }
+
+    @Override
+    public void onAuthenticationFailed() {
+      Log.e(TAG, "验证失败");
+      promise.reject("EXCEPTION", "验证失败");
+    }
+  }
 }
