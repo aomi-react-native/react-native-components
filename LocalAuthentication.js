4ab3f0dcae820c3c234224105e9bedefe1bbc47d
Sean <sean.snow@live.com>
2017-10-09 16:43:32 +0800

    backup

--- LocalAuthentication.js
+++ LocalAuthentication.js
@@ -1,6 +1,22 @@
-import { NativeModules, Platform } from 'react-native';
+import React from 'react';
+import { NativeModules, Platform, View } from 'react-native';
+import modal from './modal';
+import SvgIcon from './SvgIcon';
+import fingerprint from './assets/svg/fingerprint.svg';
+
 
 const {SitbLocalAuthentication} = NativeModules;
+
+const styles = {
+  content: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    margin: 15,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 export const Code = {
   systemCancel: 'SystemCancel',
@@ -30,7 +46,16 @@ export default class LocalAuthentication {
     if (Platform.OS !== 'android') {
       return SitbLocalAuthentication.fingerprintValidate(msg);
     }
-
+    modal({
+      content: (
+        <View style={styles.content}>
+          <SvgIcon source={fingerprint}
+                   width={50}
+                   height={50}
+          />
+        </View>
+      )
+    });
     return SitbLocalAuthentication.fingerprintValidate(msg);
   }
 
--- modal.js
+++ modal.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Component from './AbstractComponent';
 import { Text, View } from 'react-native';
-import createRootView from './createRootView';
+import { createRootView } from './createRootNode';
 import { AbstractDialog } from './Dialog';
 import Button from './Button';
 
 const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,.4)'
+  },
   buttonGroup: {}
 };
 
@@ -18,6 +23,78 @@ const variables = {
   }
 };
 
+class SceneModal extends Component {
+
+  state = {
+    visible: true
+  };
+
+  handleOkPress() {
+    const {onOk} = this.props;
+    const result = onOk && onOk();
+    this.handleClose(result);
+  }
+
+  handleCancelPress() {
+    const {onCancel} = this.props;
+    const result = onCancel && onCancel();
+    this.handleClose(result);
+  }
+
+  handleDismiss() {
+    const {onDismiss} = this.props;
+    const result = onDismiss && onDismiss();
+    this.handleClose(result);
+  }
+
+  handleClose(result) {
+    if (result !== true) {
+      this.setState({visible: false});
+    }
+  }
+
+  render() {
+    const {title, content, onCancel, onOk} = this.props;
+    const {
+      config: {
+        okText,
+        cancelText,
+        okButtonProps,
+        cancelButtonProps
+      }
+    } = variables;
+    return (
+      <AbstractDialog onPress={this.handleDismiss}
+                      style={styles.container}
+                      visible={this.state.visible}
+      >
+        {typeof title && (
+          <Text>{title}</Text>
+        )}
+        {typeof content === 'string' ? (
+          <Text>{content}</Text>
+        ) : content}
+        <View style={styles.buttonGroup}>
+          {onCancel && (
+            <Button {...cancelButtonProps}
+                    onPress={this.handleCancelPress}
+            >
+              {cancelText}
+            </Button>
+          )}
+          {onOk && (
+            <Button {...okButtonProps}
+                    onPress={this.handleOkPress}
+            >
+              {okText}
+            </Button>
+          )}
+        </View>
+      </AbstractDialog>
+    );
+  }
+}
+
 /**
  * 情景模态框
  * @param scene
@@ -25,74 +102,23 @@ const variables = {
  * @param content
  * @param onOk
  * @param onCancel
+ * @param onDismiss
  */
-function sceneModal({scene, title = '', content, onOk, onCancel, onDismiss}) {
-  const {
-    config: {
-      okText,
-      cancelText,
-      okButtonProps,
-      cancelButtonProps
-    }
-  } = variables;
-
-  createRootView(class SceneModal extends Component {
-
-    state = {
-      visible: true
-    };
-
-    handleOkPress() {
-      const result = onOk && onOk();
-      this.handleClose(result);
-    }
-
-    handleCancelPress() {
-      const result = onCancel && onCancel();
-      this.handleClose(result);
-    }
-
-    handleDismiss() {
-      const result = onDismiss && onDismiss();
-      this.handleClose(result);
-    }
-
-    handleClose(result) {
-      if (result !== true) {
-        this.setState({visible: false});
-      }
-    }
-
-    render() {
-      return (
-        <AbstractDialog onPress={this.handleDismiss}
-                        visible={this.state.visible}
-        >
-          <Text>{title}</Text>
-          {typeof content === 'string' ? (
-            <Text>{content}</Text>
-          ) : content}
-          <View style={styles.buttonGroup}>
-            {onCancel && (
-              <Button {...cancelButtonProps}
-                      onPress={this.handleCancelPress}
-              >
-                {cancelText}
-              </Button>
-            )}
-            {onOk && (
-              <Button {...okButtonProps}
-                      onPress={this.handleOkPress}
-              >
-                {okText}
-              </Button>
-            )}
-          </View>
-        </AbstractDialog>
-      );
-    }
-  });
+function sceneModal({scene, title = '', content, onOk, onCancel, onDismiss} = {}) {
+  createRootView(
+    <SceneModal content={content}
+                onCancel={onCancel}
+                onDismiss={onDismiss}
+                onOk={onOk}
+                scene={scene}
+                title={title}
+    />
+  );
 }
+
+export default (args) => {
+  sceneModal(args);
+};
 
 /**
  * 消息模态框
