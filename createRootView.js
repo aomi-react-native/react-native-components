ed9539d6ab8556329fca3bcc9d1aa76e1965d533
Sean <sean.snow@live.com>
2017-10-09 15:04:56 +0800

    代码优化，新增props 和 manager

--- createRootView.js
+++ createRootView.js
@@ -6,15 +6,35 @@ import RootSiblings from 'react-native-root-siblings';
  * @author 田尘殇Sean(sean.snow@live.com)
  * @date 16/8/11
  */
-export default function (RooView) {
+export default function (RooView, props) {
+
   return class extends Component {
 
+    static propTypes = RooView.prototype;
+
+    manager;
+
+    constructor(props) {
+      super(props);
+      this.manager = new RootSiblings(<View/>);
+    }
+
     componentDidMount() {
-      this.manager = new RootSiblings(<RooView {...this.props}/>);
+      this.manager.update(
+        <RooView {...props}
+                 {...this.props}
+                 manager={this.manager}
+        />
+      );
     }
 
     componentDidUpdate() {
-      this.manager.update(<RooView {...this.props}/>);
+      this.manager.update(
+        <RooView {...props}
+                 {...this.props}
+                 manager={this.manager}
+        />
+      );
     }
 
     componentWillUnmount() {
@@ -22,7 +42,7 @@ export default function (RooView) {
     }
 
     render() {
-      return <View />;
+      return <View/>;
     }
   };
 }
