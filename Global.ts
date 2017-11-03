93992ca532183b424cee0fbd794f2e19fc57a4fd
Sean <sean.snow@live.com>
2017-11-04 03:56:35 +0800

    BaseProps

--- Global.ts
+++ Global.ts
@@ -1,12 +1,17 @@
-import * as React from 'react';
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2017/11/4
  */
+import { ReactNode } from 'react';
+
 export type EmptyFunction = () => void;
 
-export type ReactNodeFunction = () => React.ReactNode;
+export type ReactNodeFunction = () => ReactNode;
 
 export interface Size {
   width?: number;
   height?: number
+}
+
+export interface BaseProps {
+  children?: ReactNode;
 }
