571e1960037f6e7cff0443fa272962d143094764
Sean <sean.snow@live.com>
2017-11-04 00:23:16 +0800

    组件功能优化和转换为ts实现

---
+++ Input/Props.ts
@@ -0,0 +1,36 @@
+import { BaseFormPropTypes } from "../Form/AbstractFormComponent";
+
+/**
+ * @author Sean sean.snow@live.com createAt 2017/11/3
+ */
+export default interface PropTypes extends BaseFormPropTypes {
+
+  /**
+   * input 默认值
+   */
+  defaultValue?: string;
+
+  pattern?: RegExp;
+
+  required?: boolean;
+
+  value?: string;
+
+  /**
+   * 是否启用表单验证,默认为true
+   */
+  validate?: boolean;
+
+  /**
+   * 验证字段的值是否正确
+   * @param {string} text input 的值
+   * @returns {boolean} 是否符合预期值
+   */
+  valid?: (text: string) => boolean;
+
+  inputStyle?: any;
+
+  style?: any;
+
+  onChangeText?: (text: string) => void
+}
---
+++ Input/index.tsx
@@ -0,0 +1,186 @@
+import * as React from 'react';
+import AbstractFormComponent from '../Form/AbstractFormComponent';
+import { Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+import commonStyle, { Colors, fontSize, separatorHeight } from '../styles';
+
+import Props from './Props';
+
+// noinspection JSSuspiciousNameCombination
+const styles = StyleSheet.create<any>({
+  container: {
+    flexDirection: 'row',
+    borderWidth: separatorHeight,
+    borderColor: Colors.separator,
+    backgroundColor: Colors.white
+  },
+  icon: {
+    ...commonStyle.center,
+    width: 25
+  },
+  input: {
+    flex: 1,
+    minHeight: 40,
+    marginTop: Platform.OS === 'ios' ? 2 : 0
+  },
+  textInput: {
+    fontSize,
+    color: Colors.fontColor
+  },
+  label: {
+    justifyContent: 'center',
+    marginRight: 10
+  }
+});
+
+const INPUT_PROPS_KEYS = [
+  'autoCapitalize',
+  'autoCorrect',
+  'autoFocus',
+  'defaultValue',
+  'editable',
+  'keyboardType',
+  'maxLength',
+  'multiline',
+  'onBlur',
+  'onChange',
+  'onChangeText',
+  'onEndEditing',
+  'onFocus',
+  'onSubmitEditing',
+  'placeholder',
+  'placeholderTextColor',
+  'secureTextEntry',
+  'value',
+  'blurOnSubmit',
+  'clearButtonMode',
+  'clearTextOnFocus',
+  'enablesReturnKeyAutomatically',
+  'keyboardAppearance',
+  'numberOfLines',
+  'onKeyPress',
+  'returnKeyType',
+  'selectTextOnFocus',
+  'selectionState',
+  'textAlign',
+  'underlineColorAndroid',
+  'name'
+];
+
+export default class Input extends AbstractFormComponent<Props, any> {
+
+
+  static defaultProps = {
+    defaultValue: '',
+    underlineColorAndroid: 'transparent',
+    validate: true
+  };
+
+  state;
+
+  // refs
+  textInput;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue || ''
+    };
+
+    const {name, form} = this.props;
+    name && form && form.putFormValue(name, this.props.defaultValue);
+    this.putFormField();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.defaultValue !== this.props.defaultValue) {
+      this.handleChangeText(nextProps.defaultValue);
+    }
+  }
+
+  componentDidUpdate() {
+    this.putFormField();
+  }
+
+  putFormField() {
+    const {name, form} = this.props;
+    if (name && form) {
+      form.formFields[name] = this;
+    }
+  }
+
+  getValue(): any {
+    return this.state.value;
+  }
+
+  isValid(): boolean {
+    let {validate, valid, pattern, required, name, form} = this.props;
+    if (validate) {
+      if (valid) {
+        let result = valid(this.state.value);
+        this.setFormFieldInfo(result);
+        return result;
+      }
+
+      if (required) {
+        if (this.state.value === '') {
+          name && form && form.putMissField(name);
+          return false;
+        }
+        name && form && form.deleteErrOrMissField(name);
+      }
+
+      if (pattern) {
+        let result = pattern.test(this.state.value);
+        this.setFormFieldInfo(result);
+        return result;
+      }
+    }
+
+    return true;
+  }
+
+  setFormFieldInfo(result) {
+    const {name, form} = this.props;
+    if (result) {
+      name && form && form.deleteErrOrMissField(name);
+    } else {
+      name && form && form.putErrorField(name);
+    }
+  }
+
+  handleChangeText(value) {
+    this.setState({value});
+    const {onChangeText, name, form} = this.props;
+    name && form && form.putFormValue(name, value);
+    onChangeText && onChangeText(value);
+  }
+
+  focus() {
+    this.textInput.focus();
+  }
+
+  render() {
+    let {style, inputStyle, ...other} = this.props;
+
+    let newInputProps = {};
+    let newProps = {...other};
+
+    INPUT_PROPS_KEYS.forEach(key => {
+      newInputProps[key] = this.props[key];
+      newProps[key] && Reflect.deleteProperty(newProps, key);
+    });
+
+    return (
+      <View {...newProps}
+            style={[styles.container, style]}
+      >
+        <TextInput {...newInputProps}
+                   onChangeText={this.handleChangeText}
+                   ref={textInput => this.textInput = textInput}
+                   style={[styles.input, inputStyle]}
+        />
+      </View>
+    );
+  }
+
+}
