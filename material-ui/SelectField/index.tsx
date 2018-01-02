38d8af6f3b6e15f0bde6d71ea03a8a086b79c50a
Sean <sean.snow@live.com>
2018-01-02 13:55:00 +0800

    init

---
+++ material-ui/SelectField/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import TextInput from '../TextField';
+import { Props } from '../TextField/Props';
+
+/**
+ *
+ * @author 田尘殇Sean(sean.snow@live.com) create at 2018/1/2
+ */
+export default class SelectField extends React.Component<Props> {
+
+  render() {
+    return (
+      <TextInput {...this.props}
+                 type='select'
+      />
+    );
+  }
+
+}
