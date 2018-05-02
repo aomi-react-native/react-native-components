911fe28ae307d3c5a6778f8c913e0709f790c8cf
Sean <sean.snow@live.com>
2018-05-02 15:29:32 +0800

    extends ViewProps

--- Form/Props.ts
+++ Form/Props.ts
@@ -1,11 +1,7 @@
-import * as React from 'react';
-import Form from './index';
-import { StyleProp, ViewStyle } from 'react-native';
+import { ViewProps } from 'react-native';
 
 /**
  * @author 田尘殇Sean(sean.snow@live.com) create at 2017/11/4
  */
-export interface Props {
-  renderChildren: (form: Form) => React.ReactNode;
-  style?: StyleProp<ViewStyle>;
+export interface Props extends ViewProps {
 }
