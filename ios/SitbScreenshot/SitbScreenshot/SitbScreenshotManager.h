ab4aca778860787027e2f90181711541ba57593a
Sean <sean.snow@live.com>
2018-06-10 11:59:26 +0800

    ios 屏幕截屏监听

---
+++ ios/SitbScreenshot/SitbScreenshot/SitbScreenshotManager.h
@@ -0,0 +1,14 @@
+//
+//  SitbScreenshotManager.h
+//  SitbScreenshotManager
+//
+//  Created by 田尘殇 on 2018/6/10.
+//  Copyright © 2018年 Sitb Software. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface SitbScreenshotManager : RCTEventEmitter <RCTBridgeModule>
+
+@end
---
+++ ios/SitbScreenshot/SitbScreenshot/SitbScreenshotManager.m
@@ -0,0 +1,38 @@
+//
+//  SitbScreenshotManager.m
+//  SitbScreenshotManager
+//
+//  Created by 田尘殇 on 2018/6/10.
+//  Copyright © 2018年 Sitb Software. All rights reserved.
+//
+
+#import "SitbScreenshotManager.h"
+#import <React/RCTLog.h>
+
+@implementation SitbScreenshotManager
+
+RCT_EXPORT_MODULE(SitbScreenshotManager)
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(userDidTakeScreenshot:)
+                                                     name:UIApplicationUserDidTakeScreenshotNotification object:nil];
+    }
+
+    return self;
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"UserDidTakeScreenshot"];
+}
+
+- (void)userDidTakeScreenshot:(NSNotification *)notification {
+    RCTLog(@"检查到用户截取屏幕");
+    [self sendEventWithName:@"UserDidTakeScreenshot"
+                       body:@{@"name": @""}
+    ];
+}
+
+@end
