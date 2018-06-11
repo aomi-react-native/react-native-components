7998ec3a74497a592c8bd64b3955a6cdf12a113c
Sean <sean.snow@live.com>
2018-06-11 15:55:52 +0800

    RootManager

--- android/utils/src/main/java/software/sitb/react/utils/UtilsPackage.java
+++ android/utils/src/main/java/software/sitb/react/utils/UtilsPackage.java
@@ -2,6 +2,7 @@ package software.sitb.react.utils;
 
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+
 import software.sitb.react.commons.DefaultReactPackage;
 
 import java.util.Arrays;
@@ -13,12 +14,13 @@ import java.util.List;
  */
 public class UtilsPackage extends DefaultReactPackage {
 
-  @Override
-  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Arrays.<NativeModule>asList(
-      new VersionManager(reactContext),
-      new DeviceManager(reactContext),
-      new SmsManager(reactContext)
-    );
-  }
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(
+                new VersionManager(reactContext),
+                new DeviceManager(reactContext),
+                new SmsManager(reactContext),
+                new RootManager(reactContext)
+        );
+    }
 }
