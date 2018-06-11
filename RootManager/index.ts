f2c6888e4c5870356911bdc2cbdfa783c0a2c063
Sean <sean.snow@live.com>
2018-06-11 15:56:06 +0800

    RootManager

---
+++ RootManager/index.ts
@@ -0,0 +1,11 @@
+import { NativeModules } from 'react-native';
+
+const {SitbRootManager} = NativeModules;
+
+/**
+ * @author 田尘殇Sean(sean.snow@live.com) create at 2018/6/11
+ */
+export async function isRoot() {
+  console.log('判断手机是否root');
+  return await SitbRootManager.isRoot();
+}
