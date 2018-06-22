1abc9e2f00bd868f801e4123508edec3d359f82e
Sean <sean.snow@live.com>
2018-06-22 15:43:21 +0800

    改名字

---
+++ ios/SitbScreenshot/ScreenshotManager.h
@@ -0,0 +1,14 @@
+//
+//  ScreenshotManager.h
+//  ScreenshotManager
+//
+//  Created by 田尘殇 on 2018/6/10.
+//  Copyright © 2018年 Sitb Software. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface ScreenshotManager : RCTEventEmitter <RCTBridgeModule>
+
+@end
