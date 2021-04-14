162bc601264360c3d52331c2104212079b1e711f
sean <sean.snow@live.com>
2021-04-14 20:40:47 +0800

    feat: 返回类型设置

--- styles/useThemeProps.ts
+++ styles/useThemeProps.ts
@@ -2,7 +2,29 @@ import { useTheme } from './useTheme';
 import defaultTheme from './defaultTheme';
 import { getThemeProps } from './getThemeProps';
 
-export function useThemeProps({ props, name }) {
+export interface ThemeWithProps {}
+
+export type ThemedProps<Theme, Name extends keyof any> = Theme extends {
+  components: Record<Name, { defaultProps: infer Props }>;
+}
+  ? Props
+  : {};
+
+export interface AdditionalThemeProps<Theme> {
+  theme: Theme;
+}
+
+export function useThemeProps<
+  Theme extends ThemeWithProps,
+  Props,
+  Name extends keyof any
+>({
+  props,
+  name
+}: {
+  props: Props;
+  name: Name;
+}): Props & ThemedProps<Theme, Name> & AdditionalThemeProps<Theme> {
   const contextTheme = useTheme() || defaultTheme;
   const more = getThemeProps({ theme: contextTheme, name, props });
   const theme = more.theme || contextTheme;
